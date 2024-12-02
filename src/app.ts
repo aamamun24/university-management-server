023ae182bde969ad5d0eaf14c1b3eee423f21dad
Abdullah Al Mamun <bdmamun100@gmail.com>
2024-12-02 14:43:46 +0600

    project setup completed and get data

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoute } from './app/modules/student/student.route';
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+// application routes
+app.use('/api/v1/students', StudentRoute);
+
+const getHelloController = (req: Request, res: Response) => {
   const hello = 'Hello World!';
   res.send(hello);
-});
+};
+
+app.get('/', getHelloController);
 
 export default app;
---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+export interface Guardian {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+}
+
+export interface UserName {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+}
+
+export interface LocalGuardian {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+}
+
+export interface Student {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dob?: string;
+  email: string;
+  contactNo: string;
+  emergencyContact: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: boolean;
+}
